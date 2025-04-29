ab226b5b682069e1e44f50a60e6c2cf0bc6be1e9
Uday Bhan Singh <uday@biz4group.com>
2025-04-29 16:59:09 +0530

    removed uuid package

--- src/ListManager.tsx
+++ src/ListManager.tsx
@@ -1,8 +1,26 @@
 import { DragAndDropWrapper } from "./DragAndDropWrapper/DragAndDropWrapper";
 import { withMaxItems } from "./withMaxItems/withMaxItems";
 import { withReactToItemsChange } from "./withReactToItemsChange/withReactToItemsChange";
-import { v4 as uuid } from "uuid";
 
+
+function uuid(): string {
+
+    const array: Uint8Array = new Uint8Array(6);
+    const length = 6;
+    const bytes = new Uint8Array(length);
+
+    // Generate random values
+    for (let i = 0; i < length; i++) {
+        bytes[i] = Math.floor(Math.random() * 256);
+    }
+
+    // Copy to the original array
+    for (let i = 0; i < length; i++) {
+        array[i] = bytes[i];
+    }
+
+    return array.join('');
+}
 const ComponentWithMaxItems = withMaxItems(DragAndDropWrapper, uuid);
 const ComponentWithReactToItemsChange = withReactToItemsChange(ComponentWithMaxItems);
 
